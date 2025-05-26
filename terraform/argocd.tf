c4cd74b2235e26e3c0dd46a79b317f3b6e04398f
sean <you@example.com>
2025-05-25 17:13:33 -0700

    refactor: shifted manifests to new repo, added application

---
+++ terraform/argocd.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = "argocd"
+  create_namespace = true
+  version    = "8.0.9"
+
+  values = [file("values.yaml")]
+}
+
